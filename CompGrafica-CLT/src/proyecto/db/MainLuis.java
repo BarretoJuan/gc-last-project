387c8d867160be1ddc5c9ade9398c362685ea954
Luis Garrillo <lggarrillo@urbe.edu.ve>
2024-03-18 22:55:18 -0400

    added function to get the reports made by a given user and created the Report class

--- CompGrafica-CLT/src/proyecto/db/MainLuis.java
+++ CompGrafica-CLT/src/proyecto/db/MainLuis.java
@@ -7,7 +7,7 @@ public class MainLuis {
 	public static void main(String[] args) throws SQLException {
 		// AMO JAVA
 		Caller caller = new Caller();
-		caller.insertClient("Luis Garrillo", "10448405", "hola", "lgg", "email@gmail.com");
+		caller.getUserReports(1);
 		caller.endConnection();
 	}
 	
