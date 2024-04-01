463b5b7e3854ea9964cde9ac613bc94b5debc229
Luis Garrillo <lggarrillo@urbe.edu.ve>
2024-03-31 23:32:29 -0400

    setting up content verifications

---
+++ CompGrafica-CLT/src/proyecto/verifications/ContentVerifications.java
@@ -0,0 +1,40 @@
+package proyecto.verifications;
+
+public class ContentVerifications {
+	
+	public boolean verifyUsername(String username) {
+		
+		return true;
+	}
+	
+	public boolean verifyName(String name) {
+		
+		return true;
+	}
+	
+	public boolean verifyCedula(String cedula) {
+		
+		return true;
+	}
+	
+	public boolean verifyEmail(String email) {
+		
+		return true;
+	}
+	
+	public boolean verifyTelephone(String telephone) {
+		
+		return true;
+	}
+	
+	public boolean verifyTitle(String title) {
+		
+		return true;
+	}
+	
+	public boolean verifyBody(String body) {
+		
+		return true;
+	}
+	
+}
